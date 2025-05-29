0003aafd5daddca38cd23fd4662909cd389a7e7b
Filip2k03 <stephanfilip7@gmail.com>
2025-05-29 15:56:03 +0630

    toaster add

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { ToastProvider } from "./components/ToastProvider";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
+  <ToastProvider>
     <App />
+  </ToastProvider>
   </BrowserRouter>
 );
