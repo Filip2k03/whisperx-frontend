12f8a9ed53d1100356fbbe15759c34d558278270
Filip2k03 <stephanfilip7@gmail.com>
2025-05-30 21:33:35 +0630

    ad

--- src/AdComponent.tsx
+++ src/AdComponent.tsx
@@ -1,15 +1,15 @@
-// src/AdComponent.tsx
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const AdComponent = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
   useEffect(() => {
-    // Dynamically load the Ad script or iframe
     const script = document.createElement('script');
     script.src = 'https://www.profitableratecpm.com/w80rzsx4b?key=8b4763ad6ff8ff8676cac22bb5e506b6';
     script.async = true;
+    script.onload = () => setIsLoading(false);
     document.body.appendChild(script);
 
-    // Cleanup the script when the component is unmounted
     return () => {
       document.body.removeChild(script);
     };
@@ -17,7 +17,11 @@ const AdComponent = () => {
 
   return (
     <div className="ad-container">
-      <p>Ad will be displayed here!</p>
+      {isLoading ? (
+        <p>Loading ad...</p>
+      ) : (
+        <p>Ad is displayed here!</p>
+      )}
     </div>
   );
 };
